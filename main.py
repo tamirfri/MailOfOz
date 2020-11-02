d2c97e715a34f3f87ca58fdf60a8703f0eeec406
Tamir Friedman <tamir.fri@gmail.com>
2020-11-02 13:14:09 +0000

    added tests

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from mail_system import parse_all_system
 from mail_ops import parse_all_ops
 
-if __name__ == '__main__':
+def main():
   cities = parse_all_system()
   parse_all_ops(cities)
+
+if __name__ == '__main__':
+  main()
