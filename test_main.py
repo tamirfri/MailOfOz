07f3f2da7a9202534e7377bc710e4b5c1624601c
Tamir Friedman <tamir.fri@gmail.com>
2020-11-02 15:55:37 +0200

    fixed pytest issue

--- test_main.py
+++ test_main.py
@@ -4,12 +4,12 @@ from pathlib import Path
 
 def test_parse():
   assert parse_int("6") == 6
-  assert parse_int("0") == 6
+  assert parse_int("0") == 0
   assert parse_pack("PACK 0") == ('PACK', 0)
 
 def test_main():
   test_input = Path('test_input.txt').read_text()
   test_output = Path('test_output.txt').read_text()
-  completed_process = run('main.py', input=test_input, text=True)
+  completed_process = run(('python3', './main.py'), input=test_input, text=True, capture_output=True)
   completed_process.check_returncode()
   assert test_output == completed_process.stdout
